c8d8f7440eda140c32874b4e688e2ffc3cca55cb
Ashley Baldry <ashley.baldry@kindredgroup.com>
2020-01-30 16:05:01 +0000

    Stage 1 complete

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Track from '../Track';
+
+export class TrackList extends React.Component {
+    render() {
+        return (
+            <div className="TrackList">
+            {
+                this.props.tracks.map(track => {
+                    return <Track track={track} key={track.id}/>
+                })
+            }  
+            </div>
+        );
+      };
+}
